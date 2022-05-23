6a858212cccefaa96f242ca17d84b872e99a92cf
joshuayu1990 <348756832@gapps.yrdsb.ca>
2022-05-23 12:37:53 -0400

    Animated Elephant Idle

--- Elephant.java
+++ Elephant.java
@@ -13,6 +13,26 @@ public class Elephant extends Actor
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     GreenfootSound elephantSound = new GreenfootSound("ahhhhhhhhhhhhhhhhh.wav");
+    GreenfootImage[] idle = new GreenfootImage[8];
+    
+    public Elephant()
+    {
+        for(int i = 0; i < 8; i++)
+        {
+            idle[i] = new GreenfootImage("images/elephant_idle/idle" + i + ".png");
+        }
+        setImage(idle[0]);
+    }
+    /** 
+     * Animate Elephant
+     */
+    int imageIndex = 0;
+    public void animateElephant()
+    {
+        setImage(idle[imageIndex]);
+        imageIndex = (imageIndex + 1) % idle.length;
+    }
+    
     public void act()
     {
         if(Greenfoot.isKeyDown("left"))
@@ -26,6 +46,9 @@ public class Elephant extends Actor
         
         // Remove apple if touched
         eat();
+        
+        // Animate elephant
+        animateElephant();
     }
     
     /**
