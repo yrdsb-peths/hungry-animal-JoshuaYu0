21199e608fb630c0df3407f69084deb621a4f5e8
joshuayu1990 <348756832@gapps.yrdsb.ca>
2022-05-24 10:28:18 -0400

    Added Level & Speed

--- Apple.java
+++ Apple.java
@@ -12,10 +12,11 @@ public class Apple extends Actor
      * Act - do whatever the Apple wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    int speed = 1;
     public void act()
     {
         // Add your action code here.
-        setLocation(getX(), getY() + 3);
+        setLocation(getX(), getY() + speed);
         
         // Remove apple and show game over.
         MyWorld world = (MyWorld) getWorld();
@@ -24,5 +25,10 @@ public class Apple extends Actor
             world.gameOver();
             world.removeObject(this);
         }
+    }
+    
+    public void setSpeed(int spd)
+    {
+        speed = spd;
     }
 }
--- TitleScreen.java
+++ TitleScreen.java
@@ -34,6 +34,7 @@ public class TitleScreen extends World
             Greenfoot.setWorld(gameWorld);
         }
     }
+    
     /**
      * Prepare the world for the start of the program.
      * That is: create the initial objects and add them to the world.
@@ -43,5 +44,22 @@ public class TitleScreen extends World
         Elephant elephant = new Elephant();
         addObject(elephant,524,52);
         elephant.setLocation(300,343);
+        Label label = new Label("Press <space> to Start", 40);
+        addObject(label,254,124);
+        label.setLocation(391,264);
+        label.setLocation(317,130);
+        Label label2 = new Label("Use \u2190 and \u2192 to Move", 40);
+        addObject(label2,254,186);
+        label2.setLocation(334,191);
+        label2.setLocation(326,244);
+        label.setLocation(329,191);
+        label.setLocation(314,154);
+        label2.setLocation(319,207);
+        label.setLocation(294,100);
+        label2.setLocation(422,132);
+        removeObject(label2);
+        Label label3 = new Label("Use <-- and --> to Move", 45);
+        addObject(label3,264,145);
+        label3.setLocation(308,267);
     }
 }
