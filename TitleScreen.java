2d8370b9119fe9a850490b4040c140bd86d83706
joshuayu1990 <348756832@gapps.yrdsb.ca>
2022-05-23 21:54:30 -0400

    Title Screen

--- TitleScreen.java
+++ TitleScreen.java
@@ -17,10 +17,11 @@ public class TitleScreen extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
-        
+
         addObject(titleLabel, getWidth()/2, getHeight()/2);
+        prepare();
     }
-    
+
     /**
      * The main world act loop.
      */
@@ -32,5 +33,15 @@ public class TitleScreen extends World
             MyWorld gameWorld = new MyWorld();
             Greenfoot.setWorld(gameWorld);
         }
+    }
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Elephant elephant = new Elephant();
+        addObject(elephant,524,52);
+        elephant.setLocation(300,343);
     }
 }
